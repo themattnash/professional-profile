1a9ebc7a3bb70ee329f758b1981b855f7b86903a
Matt Nash <matt@thenashfamily.io>
2025-09-03 17:18:58 -0400

    Footer update 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
 			<div className="container-custom">
 				<div className="flex flex-col md:flex-row justify-between items-center gap-4">
 					<p className="text-background/60">
-						© {currentYear} All rights reserved.
+						© {currentYear}, Matt Nash. All rights reserved.
 					</p>
 					<div className="flex gap-6 text-background/60">
 						<span>&#127757; Built by Earthlings</span>
