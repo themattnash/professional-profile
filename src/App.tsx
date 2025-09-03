7aae4d166e008421d3e985d3e523bf62068bebea
Matt Nash <matt@thenashfamily.io>
2025-09-03 17:00:08 -0400

    Remove duplicate BrowserRouter; use routes under main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ErrorBoundary from "@/components/ErrorBoundary";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { Routes, Route } from 'react-router-dom';
+import ErrorBoundary from '@/components/ErrorBoundary';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => (
   <ErrorBoundary>
-    <BrowserRouter basename="/my-portfolio">
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   </ErrorBoundary>
 );
 
