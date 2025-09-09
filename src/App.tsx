5a145b7b0d314dafdb11ed5b8eba677551e6cce1
Matt Nash <matt@thenashfamily.io>
2025-09-09 11:42:47 -0400

    feat: remember and restore scroll position on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ErrorBoundary from '@/components/ErrorBoundary';
+import ScrollMemory from '@/components/ScrollMemory';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
 
 const App: React.FC = () => (
   <ErrorBoundary>
+    <ScrollMemory />
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="*" element={<NotFound />} />
